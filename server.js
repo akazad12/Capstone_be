03c50b23eba13c89156dd2f9414abdd9d8c5eac4
akazad12 <akazad12@aim.com>
2026-03-06 10:42:53 -0500

    imported and implemented cors

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 //Imports
 import express from 'express';
 import dotenv from 'dotenv';
-import {globalErr, logReq} from "./middleware/middlewares.js"
-import connectDB from "./db/conn.js"
-import portfolioRoutes from "./routes/portfolioRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import assetRoutes from "./routes/assetRoutes.js"
-import transactionRoutes from "./routes/transactionRoutes.js"
+import {globalErr, logReq} from "./middleware/middlewares.js";
+import connectDB from "./db/conn.js";
+import portfolioRoutes from "./routes/portfolioRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import assetRoutes from "./routes/assetRoutes.js";
+import transactionRoutes from "./routes/transactionRoutes.js";
 import watchListRoutes from "./routes/watchListRoutes.js";
+import cors from "cors";
+
 //Setups
 dotenv.config()
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 connectDB();
 
 //Middleware
+app.use(cors());
 app.use(express.json());
 app.use(logReq);
 
